b5537f6ba2353698e2989c6d7e15c6a68c3d72ea
Rong Sun <sun.rongyue@outlook.com>
2023-06-19 17:56:16 +1200

    work on fact list component

--- src/components/FactList.jsx
+++ src/components/FactList.jsx
@@ -1,4 +1,20 @@
-function FactList() {
-  return <section>Fact List</section>;
+function FactList(props) {
+  return (
+    <section>
+      <ul className="fact-list">
+        <li className="fact">
+          <p>
+            {props.text}
+            <a className="source" href={props.source}>
+              (source)
+            </a>
+          </p>
+          <span className="tag" style={{ backgroundColor: "#3b82f6" }}>
+            {props.category}
+          </span>
+        </li>
+      </ul>
+    </section>
+  );
 }
 export default FactList;
