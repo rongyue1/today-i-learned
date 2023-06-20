fa50d82b2ee2d1c684b6e22490036b23f7a60cdd
Rong Sun <sun.rongyue@outlook.com>
2023-06-21 11:42:43 +1200

    add Fact component

---
+++ src/components/Fact.jsx
@@ -0,0 +1,42 @@
+function Fact({ props }) {
+  const CATEGORIES = [
+    { name: "technology", color: "#3b82f6" },
+    { name: "science", color: "#16a34a" },
+    { name: "finance", color: "#ef4444" },
+    { name: "society", color: "#eab308" },
+    { name: "entertainment", color: "#db2777" },
+    { name: "health", color: "#14b8a6" },
+    { name: "history", color: "#f97316" },
+    { name: "news", color: "#8b5cf6" },
+  ];
+
+  function findColor(categoryName) {
+    return categoryName.name === props.category;
+  }
+
+  return (
+    <ul className="fact-list">
+      <li className="fact">
+        <p>
+          {props.text}
+          <a className="source" href={props.source}>
+            (source)
+          </a>
+        </p>
+        <span
+          className="tag"
+          style={{ backgroundColor: CATEGORIES.find(findColor).color }}
+        >
+          {props.category}
+        </span>
+        <div className="vote-buttons">
+          <button>👍 {props.votesInteresting}</button>
+          <button>🤯 {props.votesMindblowing}</button>
+          <button>⛔️ {props.votesFalse}</button>
+        </div>
+      </li>
+    </ul>
+  );
+}
+
+export default Fact;
